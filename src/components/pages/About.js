3de84938f7a24336c09684da281e1b808e709db5
Jing Li <lijing900913@gmail.com>
2022-02-02 00:21:13 -0500

    Add contact and resume

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,16 +2,12 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-    </div>
+    <section class="about-me" id="about-me">
+      <h1>Resume</h1>
+       <div></div>
+       <figure className="resume-img"></figure>
+      
+       
+    </section>
   );
 }
