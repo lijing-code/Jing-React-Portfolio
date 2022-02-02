fee470fab78ab674abe236e1f4fd930b6fd19a5c
Jing Li <lijing900913@gmail.com>
2022-02-02 01:24:10 -0500

    Revise the format for each page title

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,9 +4,9 @@ export default function About() {
   return (
     <section class="about-me" id="about-me">
       <h1>Resume</h1>
-       <div></div>
+       <div>
        <figure className="resume-img"></figure>
-      
+      </div>
        
     </section>
   );
--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -3,22 +3,57 @@ import React from 'react';
 export default function Blog() {
   return (
     <div>
-      <h1>Blog Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+      <section class="work" id="work">
+        <h1>Work</h1>
+        <div class="projects">
+          {/* Project One */}
+            <a class="project" href="https://bmorrissey34.github.io/Movies-TV-Search/">
+                <figure>
+                    <img src='./image/MoviesTV Shows.png' alt="Team Project One"/>
+                    <h3>Movies/TV Shows Search</h3>
+                    <span>HTML/CSS/JavaScript</span>   
+                </figure>
+            </a>
+            <a href='https://github.com/Bmorrissey34/Movies-TV-Search'><button>GitHub Repository</button></a>
+          {/* Project Two */}
+          <a class="project" href="https://lijing-code.github.io/03-JS-Password-Generator/">
+                    <figure>
+                        <img src="./asset/image/Password Generator.png" alt="Password Generator"/> 
+                        <h3>Password Generator</h3>
+                        <span>JavaScript</span>
+                    </figure>
+                </a>  
+                <a href='https://github.com/lijing-code/03-JS-Password-Generator'><button>GitHub Repository</button></a>
+          {/* Project Three */}
+          <a class="project" href="https://lijing-code.github.io/04-Web-APIs-Code-Quiz/">
+                    <figure>
+                        <img src="./asset/image/Code Quiz.png" alt="Code Quiz"/>
+                        <h3>Code Quiz</h3>
+                        <span>Web APIs</span>
+                    </figure>
+                </a>  
+                <a href='https://github.com/lijing-code/04-Web-APIs-Code-Quiz'><button>GitHub Repository</button></a> 
+          {/* Project Four */}
+          <a class="project"  href=" https://lijing-code.github.io/05-Third-Party-APIs-Work-Day-Scheduler/">
+                    <figure>
+                        <img src="./asset/image/Work Day.png" alt="Work Day Scheduler"/>
+                        <h3>Work Day Scheduler</h3>
+                        <span>Third Party APIs</span>
+                    </figure>
+                </a>
+                <a href='https://github.com/lijing-code/05-Third-Party-APIs-Work-Day-Scheduler'><button>GitHub Repository</button></a>
+          {/* Project Five */}
+          <a class="project" href="https://lijing-code.github.io/06-Server-Side-APIs-Weather-Dashboard/">
+                    <figure>
+                        <img src="./asset/image/Weather Dashboard.png" alt="Weather Dashboard"/>
+                        <h3>Weather Dashboard</h3>
+                        <span>Server Side APIs</span>
+                    </figure>
+                </a>
+                <a href='https://github.com/lijing-code/Server-Side-APIs-Weather-Dashboard'><button>GitHub Repository</button></a>
+            </div>
+          
+      </section>
     </div>
   );
 }
